a3d7c3da697799bc47663ee18da57af804d7d9fe
mktcode <kontakt@markus-kottlaender.de>
2022-12-20 14:02:52 +0100

    removed unused var

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { useWeb3 } from "@/composables/useWeb3";
 import WelcomeModal from "@/components/WelcomeModal.vue";
 import CreateProposalModal from "@/components/CreateProposalModal.vue";
 import TheHeader from "@/components/TheHeader.vue";
 import ProposalFeed from "@/components/ProposalFeed.vue";
 
 const isCreateProposalModalOpen = ref(false);
-
-const { account } = useWeb3();
 </script>
 
 <template>

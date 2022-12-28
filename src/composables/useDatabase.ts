6171744cafecf48b7f69c4422fcc5326b4d74787
mktcode <kontakt@markus-kottlaender.de>
2022-12-28 12:51:10 +0100

    fixed typescript issue

--- src/composables/useDatabase.ts
+++ src/composables/useDatabase.ts
@@ -7,8 +7,8 @@ import { ref } from "vue";
 const gun = Gun({ peers: [import.meta.env.VITE_GUN_PEER] });
 
 const db = gun
-  .get(import.meta.env.VITE_GUN_DB_NAME)
-  .get(import.meta.env.VITE_DAO_ADDRESS);
+  .get(import.meta.env.VITE_GUN_DB_NAME as string)
+  .get(import.meta.env.VITE_DAO_ADDRESS as string);
 
 const user = ref<any>(null);
 const userManager = gun.user();

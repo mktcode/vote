ba3d7c7e83e75c40da8180362e9fcafd624c254e
mktcode <kontakt@markus-kottlaender.de>
2022-12-28 13:09:01 +0100

    removed newProposalCount

--- src/components/ProposalFeed.vue
+++ src/components/ProposalFeed.vue
@@ -15,32 +15,10 @@ db.get("proposals")
   });
 
 const isFinalizeModalOpen = ref(false);
-const newProposalsCount = ref(1);
 </script>
 
 <template>
   <div class="flex flex-col">
-    <TransitionRoot
-      appear
-      :show="newProposalsCount > 0"
-      as="template"
-      enter="transform transition duration-[400ms]"
-      enter-from="opacity-0"
-      enter-to="opacity-100"
-      leave="transform duration-200 transition ease-in-out"
-      leave-from="opacity-100 scale-100 "
-      leave-to="opacity-0 scale-95 "
-    >
-      <div class="px-3 mt-3">
-        <button
-          @click="newProposalsCount = 0"
-          class="w-full inset justify-center text-sm !text-gray-400 font-normal secondary"
-        >
-          {{ newProposalsCount }} new proposals
-        </button>
-      </div>
-    </TransitionRoot>
-
     <main
       v-if="proposalIds.length > 0"
       class="rounded-t-xl overflow-hidden mx-3 divide-y-4 divide-gray-100 shadow-lg mt-3"
